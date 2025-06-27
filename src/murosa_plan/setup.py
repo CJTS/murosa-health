974790c0c43f7bed69f963226f3520123dbbd805
Carlos Joel Tavares da Silva <carlos.joel.tavares@ibm.com>
2025-06-27 17:54:24 -0300

    remove user

--- src/murosa_plan/setup.py
+++ src/murosa_plan/setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Carlos',
-    maintainer_email='carlosjoel.tavares@gmail.com',
+    maintainer_email='user@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
